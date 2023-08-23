bb1592a01dd93cc46226541edbf68a2dac91dd7b
marc <ulg.marc@gmail.com>
2023-08-23 15:06:59 +0200

    fixed display of onyomis

--- src/components/kanjiAnsicht.js
+++ src/components/kanjiAnsicht.js
@@ -18,7 +18,7 @@ let KunyomiMeaningAssistArray = [];
 let j = 0;
 while (j < kunyomi.length) {
     let meaning = kunyomi[j].meaning;
-    KunyomiMeaningAssistArray.push(<li className="kunyomiListElement"><div className="readings">{kunyomi[j].reading_kun}</div><div className= "space"/><div>{kunyomi[j].reading_rom}</div></li>)
+    KunyomiMeaningAssistArray.push(<li className="kunyomiListElement"><div className="readings">{kunyomi[j].reading_kun}</div><div className= "space"/><div>{kunyomi[j].reading_rom}</div></li>);
     if (j < kunyomi.length-1 && kunyomi[j].meaning === kunyomi[j+1].meaning) KunyomiMeaningAssistArray.push(<div className="space">|</div>);
     else {
             KanjiReadingKun.push(<div id={`kunyomi_${j}`} className="kunyomi" key={`kunyomi_${j}`}><div className="meaning">{meaning}:</div><ul className="kunyomiList">{KunyomiMeaningAssistArray}</ul></div>);
@@ -34,10 +34,10 @@ let k = 0;
 while (k < onyomi.length) {
     let meaning = onyomi[k].meaning;
     OnyomiMeaningAssistArray.push(<li className="onyomiListElement"><div className="readings">{onyomi[k].reading_ON}</div><div className= "space"/><div>{onyomi[k].reading_rom}</div></li>);
-    if (k < onyomi.length-1 && onyomi[k].meaning === onyomi[k+1].meaning);
+    if (k < onyomi.length-1 && onyomi[k].meaning === onyomi[k+1].meaning) OnyomiMeaningAssistArray.push(<div className="space">|</div>);
     else {
-    KanjiReadingON.push(<div id={`onyomi_${k}`} className="onyomi" key={`onyomi_${k}`}><div className="meaning">{meaning}:</div><ul className="onyomiList">{OnyomiMeaningAssistArray}</ul></div>) ;
-    OnyomiMeaningAssistArray = [];
+            KanjiReadingON.push(<div id={`onyomi_${k}`} className="onyomi" key={`onyomi_${k}`}><div className="meaning">{meaning}:</div><ul className="onyomiList">{OnyomiMeaningAssistArray}</ul></div>) ;
+            OnyomiMeaningAssistArray = [];
     }
     k++;
 }
